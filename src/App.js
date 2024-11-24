2eea63eaffd1f65f43c35cd584e0a25a10c0dee7
Monnorcurphy <ConnorLMurphy@gmail.com>
2024-11-24 11:02:24 -0800

    Merge all updates for a working theme

--- src/App.js
+++ src/App.js
@@ -125,6 +125,11 @@ const App = () => {
 
             </li>
           ))}
+          {theme === 'social-media-maxxing' && (
+            <div className="social-media-maxxing-video">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/ChBg4aowzX8?si=-g0Fy7qgMgLqOngz&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+          )}
         </ul>
       </main>
     </div>
